bf3a87868a092bfd56c5b386163b3762c221a4e1
josineide <josineide1402@gmail.com>
2019-07-04 01:08:41 -0300

    alteracoes feitas

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
-cor = 0,255,0
+
+#inicializa a aplicaçao
+pygame.init()
+#cor em rgb
+cor = 0, 255, 0
+#resolucao da tela
 r = lar, alt = 640, 480
+#carregamento da tela por meio das informacoes passadas
 tela = pygame.display.set_mode(r)
 tela.fill(cor)
-pygame.init()
+#loop que mantem a tela aberta
+while True:
+    pygame.display.flip()
